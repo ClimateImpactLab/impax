51c192ec16a37d35f161808ab5c633040111ca8e
jgerardsimcock <justin_simcock@yahoo.com>
2017-10-12 16:57:44 -0700

    absolute import all

--- impax/__init__.py
+++ impax/__init__.py
@@ -3,9 +3,7 @@
 """Top-level package for impax."""
 
 from __future__ import absolute_import
-from csvv import get_gammas
 from mins import minimize_polynomial
-from impax import Impact, construct_covars, construct_weather
 
 __author__ = """Justin Simcock"""
 __email__ = 'jsimcock@rhg.com'
@@ -13,12 +11,7 @@ __version__ = '0.1.0'
 
 
 _module_imports = (
-    get_gammas,
     minimize_polynomial,
-    Impact,
-    construct_weather,
-    construct_covars,
-
 )
 
 __all__ = list(map(lambda x: x.__name__, _module_imports))
